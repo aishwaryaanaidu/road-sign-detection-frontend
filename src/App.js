ffca0c03bc7e3eba981a910162f3bf5dd5827e08
Aishwarya Naidu <aishwaryanaidu@aishwaryas-mbp.notrust.clients.wireless.pfw.edu>
2022-10-21 20:39:44 -0400

    Adding code to capture video

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [fname, setFirstName] = useState('');
+
+  const handleChange = (e) => {
+		console.log(e.target.value);
+    setFirstName(e.target.value);
+	};
+
   return (
     <div className="App">
       <header className="App-header">
@@ -9,7 +17,8 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <input type="file" accept="video/*;capture=camcorder"/>
+        <input type="file" accept="video/*;capture=camcorder" value={fname} onChange={handleChange}/>
+        <p>{fname}</p>
       </header>
     </div>
   );
